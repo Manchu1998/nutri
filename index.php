d195fdd345b3fbec10899d756405eb3109034bcd
ignacio <danis.manchu@itsae.edu.ec>
2019-11-08 09:44:15 -0500

    Avance en el sistema.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Dain</title>
+	<title>IMC</title>
 	<link rel="icon" href="assets/img/Logotipo.png" type="image/x-icon"/>
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/azzara.min.css">
--- vistas/imc.php
+++ vistas/imc.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<title>Nutri' Am</title>
+	<title>IMC</title>
 
 	<link rel="icon" href="../assets/img/Logotipo.png" type="image/x-icon"/>
 	<script src="../assets/js/plugin/webfont/webfont.min.js"></script>
